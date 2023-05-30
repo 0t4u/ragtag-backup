549a95d0525387aa32d9cbaa84e123cf3352040d
0t4u <61939142+0t4u@users.noreply.github.com>
2023-05-30 18:01:47 +0100

    h

--- cmd/version.go
+++ cmd/version.go
@@ -1,6 +1,9 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
+)
 
 func init() {
 	rootCmd.AddCommand(versionCmd)
@@ -10,4 +13,7 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Get the version of rtb",
 	Long:  "Get the version of the locally installed rtb binary",
+	Run: func(cmd *cobra.Command, args []string) {
+		log.Info().Msg("Version 1.0.0")
+	},
 }
